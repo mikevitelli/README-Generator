5816f86705e94df64c7bac950672309525ed966d
Mike <mike@mikevitelli.com>
2020-10-19 17:33:04 -0400

    stuff

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,35 @@
+//link inquirer
+var inquirer = require("inquirer");
+var fs = require("fs");
+var fileName = "goodReadMe.md";
+
 // array of questions for user
 const questions = [
-
+  //project name
+  {
+    type: "input",
+    message: "what is the title of your project?",
+    name: "projectTitle",
+  },
+  //
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  //populates the read me file
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Success!");
+  });
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then(function (response) {
+    writeToFile(fileName, response);
+  });
 }
 
 // function call to initialize program
