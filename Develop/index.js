24f839ec5c88218e3505c9dfcf16a5f2d7814373
Mike <mike@mikevitelli.com>
2020-10-19 20:00:06 -0400

    work

--- Develop/index.js
+++ Develop/index.js
@@ -1,21 +1,55 @@
 //link inquirer
 var inquirer = require("inquirer");
 var fs = require("fs");
-var fileName = "goodReadMe.md";
+
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// var fileName = "goodReadMe.md";
 
 // array of questions for user
 const questions = [
-  //project name
+  //project / input
   {
     type: "input",
     message: "what is the title of your project?",
-    name: "projectTitle",
+    name: "title",
   },
-  //
+  //description / input
+  {
+    type: "input",
+    message: "What is it?",
+    name: "description",
+  },
+  //installation / input
+  {
+    type: "input",
+    message: "How do you install it?",
+    name: "install",
+  },
+  //usage
+  {
+    type: "input",
+    message: "How do you use it?",
+    name: "usage",
+  },
+  //credits / input
+  {
+    type: "input",
+    message: "Who made it?",
+    name: "credits",
+  },
+  //license / choice
+  {
+    type: "checkbox",
+    message: "What liscense you rockin' with?",
+    name: "license",
+    choices: ["MIT", "other", "another"],
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  var fileName = "auto-generated-README.md";
   //populates the read me file
   fs.writeFile(fileName, data, function (err) {
     if (err) {
@@ -26,9 +60,9 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+function init(fileName) {
   inquirer.prompt(questions).then(function (response) {
-    writeToFile(fileName, response);
+    writeToFile(fileName, generateMarkdown(response));
   });
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,29 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.install}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+  ${data.license}
 `;
 }
 
